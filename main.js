c2185618fe748bb399dd5d32473de7ab57f83671
故乡情 <s@epnz.com>
2021-12-02 14:31:22 +0800

    添加Readme

--- main.js
+++ main.js
@@ -13,9 +13,9 @@ app.$mount()
 // #ifdef VUE3
 import { createSSRApp } from 'vue'
 export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
+    const app = createSSRApp(App)
+    return {
+        app
+    }
 }
 // #endif
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,6 +1,6 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
+		<image class="logo" src="http://static.epnz.com/icon/bhlogo.svg"></image>
 		<view class="text-area">
 			<text class="title">{{title}}</text>
 		</view>
@@ -15,7 +15,7 @@
 			}
 		},
 		onLoad() {
-
+            this.title = '别的设置'
 		},
 		methods: {
 
